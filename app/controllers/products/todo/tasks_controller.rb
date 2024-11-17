2e550b8fe94487a4d1172eb5f6689338b707356b
jonastrindade <jonastrinda@gmail.com>
2024-11-17 02:45:34 -0300

    form new and create

--- app/controllers/products/todo/tasks_controller.rb
+++ app/controllers/products/todo/tasks_controller.rb
@@ -4,6 +4,27 @@ module Products
       def index
         @tasks = policy_scope(Product::Todo::Task)
       end
+
+      def new
+        @task = Product::Todo::Task.new
+      end
+
+      def create
+        @task = Product::Todo::Task.new(create_params.merge(user_id: current_user.id))
+        if @task.save!
+          flash[:success] = "New todo successfully added!"
+          redirect_to products_todo_tasks_url
+        else
+          flash.now[:error] = "Todo creation failed"
+          render :new
+        end
+      end
+
+      private
+
+      def create_params
+        params.require(:product_todo_task).permit(:title, :description, :deadline)
+      end
     end
   end
 end
