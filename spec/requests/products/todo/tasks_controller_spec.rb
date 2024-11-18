597f7aae20b81cf630decd24e50c79676c70cf7c
jonastrindade <jonastrinda@gmail.com>
2024-11-18 00:57:12 -0300

    implement new actions spec

--- spec/requests/products/todo/tasks_controller_spec.rb
+++ spec/requests/products/todo/tasks_controller_spec.rb
@@ -13,4 +13,17 @@ RSpec.describe Products::Todo::TasksController, type: :request do
       expect(response).to render_template(:index)
     end
   end
+
+  describe "GET index" do
+    before do
+      get root_path
+    end
+
+    it "renders the index template" do
+      user = create(:user)
+      sign_in(user) if user.present?
+      get new_products_todo_task_path
+      expect(assigns(:task)).to be_a_new(Product::Todo::Task)
+    end
+  end
 end
