16cc2c68890dc1dd07d0b989af65e3ca056da9b5
jonastrindade <jonastrinda@gmail.com>
2024-11-18 02:39:03 -0300

    add tasks create specs

--- spec/requests/products/todo/tasks_controller_spec.rb
+++ spec/requests/products/todo/tasks_controller_spec.rb
@@ -14,16 +14,44 @@ RSpec.describe Products::Todo::TasksController, type: :request do
     end
   end
 
-  describe "GET index" do
+  describe "GET new" do
     before do
       get root_path
     end
 
-    it "renders the index template" do
+    it "create new task object" do
       user = create(:user)
       sign_in(user) if user.present?
       get new_products_todo_task_path
       expect(assigns(:task)).to be_a_new(Product::Todo::Task)
+    end
+  end
+
+  describe "POST create" do
+    before do
+      get root_path
+    end
+
+    it "common create task object" do
+      user = create(:user)
+      sign_in(user) if user.present?
+      post products_todo_tasks_path,
+        params: { product_todo_task: { title: Faker::Lorem.sentence, description: Faker::Lorem.paragraphs,
+        deadline: Faker::Date.forward } }
+
+      follow_redirect!
+      expect(response.body).to include("New todo successfully added!")
+    end
+
+    it "admin fail create task object" do
+      user = create(:user, role: 'admin')
+      sign_in(user) if user.present?
+      post products_todo_tasks_path,
+        params: { product_todo_task: { title: Faker::Lorem.sentence, description: Faker::Lorem.paragraphs,
+        deadline: Faker::Date.forward } }
+
+      follow_redirect!
+      expect(response.body).to include("You are not authorized to perform this action.")
     end
   end
 end
