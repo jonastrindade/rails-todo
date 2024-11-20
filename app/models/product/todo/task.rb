dd59440f6b601bf5042859d34f26fe895fd27b6e
jonastrindade <jonastrinda@gmail.com>
2024-11-20 20:01:25 -0300

    add deadline validation for type date

--- app/models/product/todo/task.rb
+++ app/models/product/todo/task.rb
@@ -6,6 +6,16 @@ module Product
       # TODO add tasks validations and tests
       # TODO add tasks callbacks and tests
       # TODO add tasks mailer and tests
+
+      validate :valid_deadline
+
+      private
+
+      def valid_deadline
+        unless deadline.is_a?(Date)
+          errors.add(:date_column, "must be a valid date")
+        end
+      end
     end
   end
 end
