d1c318430d57cd945528b8b2ab99f44f9a1febb3
jonastrindade <jonastrinda@gmail.com>
2024-11-23 18:05:00 -0300

    rubocop fix

--- db/migrate/20241123204429_add_delete_columns_for_paranoia.rb
+++ db/migrate/20241123204429_add_delete_columns_for_paranoia.rb
@@ -2,7 +2,7 @@ class AddDeleteColumnsForParanoia < ActiveRecord::Migration[8.0]
   def change
     add_column :users, :deleted_at, :datetime
     add_index :users, :deleted_at
-    
+
     add_column :tasks, :deleted_at, :datetime
     add_index :tasks, :deleted_at
   end
